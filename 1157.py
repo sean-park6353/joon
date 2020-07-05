530eb11d9e8256047e691de63b89dcdfc3bb434a
unknown <package9@naver.com>
2020-07-06 01:24:15 +0900

    단어문제풀었따

--- 1157.py
+++ 1157.py
@@ -1,12 +1,35 @@
+s = input().lower()
 
 
-s = input()
+if len(s) == 1:
+    print(s[0].upper())
+else:
 
-s = s.lower()
+    cnt = []
+    r = []
+    b = []
 
+    for i in range(len(s)):
+        b.append(s.index(s[i]))
 
-cnt = []
-for i in range(len(s)):
-    cnt.append(s.count(s[i]))
+    b = list(set(b))
+    b.sort()
+    print(b)
 
-print(cnt)
+    for i in range(len(b)):
+        cnt.append(s.count(s[b[i]]))
+        r.append(s.count(s[b[i]]))
+
+# print(cnt)
+
+    tmp = max(cnt)
+    cnt.remove(tmp)
+    tmp2 = max(cnt)
+
+    if tmp == tmp2:
+        print("?")
+    else:
+        for i in range(len(s)):
+            if max(r) == s.count(s[i]):
+                print(s[i].upper())
+                break
