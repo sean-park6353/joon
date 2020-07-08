8000439a6fe9a9cb820e7798b964266f3cc4de2c
unknown <package9@naver.com>
2020-07-09 00:39:33 +0900

    1316 다시풀어보자

--- 1316.py
+++ 1316.py
@@ -1,25 +1,8 @@
-n = int(input())
-# cnt = list()
-arr = []
-for i in range(n):
-    arr.append(input())
-    # cnt.append(0)
-
-g = []
-c = 0
-for i in range(n):
-    for j in range(len(arr[i])):
-        if arr[i].count(arr[i][j]) == 1:
-            c = c+1
-        else:
-            g.append(arr[i])
+result = int(input())
+for _ in range(result):
+    word = input()
+    for i in range(1, len(word)):
+        if word.find(word[i-1]) > word.find(word[i]):
+            result -= 1
             break
-
-r = []
-for i in range(len(g)):
-    for j in range(len(g[i])):
-        if g[i].count(g[i][j]) >= 2:
-            r.append(g[i][j])
-            break
-
-for i in ra
+print(result)
