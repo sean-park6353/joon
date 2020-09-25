ce049219dd0d3c5e976555436f2a054f3f50f15d
unknown <package9@naver.com>
2020-09-25 17:04:13 +0900

    블랙잭

--- 2798.py
+++ 2798.py
@@ -1,24 +1,23 @@
-from itertools import combinations
+N,M=list(map(int,(input().split(' '))))
+tmp=list(map(int,(input().split(' '))))
 
 
-a = list(map(int, input().split(' ')))
-N = a[0]
-M = a[1]
+num=[]
+for i in range(N):
+    for j in range(i+1,N):
+        for k in range(j+1,N):
+            num.append(tmp[i]+tmp[j]+tmp[k])
 
-card = list(map(int, input().split(' ')))
-arr = list(combinations(card, 3))
+d=3000000
+result=0
+m=[]
+for i in range(len(num)):
+    if d>abs(M-num[i]):
+        d=abs(M-num[i])
 
-print(arr)
-min_value = sum(max(arr))
-min_tuple = max(arr)
-print(min_tuple)
-tmp_tuple = ()
-tmp_value = 1
-while min_value > M:
-    arr.remove(min_tuple)
-    min_value = sum(max(arr))
-    min_tuple = max(arr)
-    tmp_tuple = min_tuple
-    tmp_value = min_value
 
-print(tmp_value)
+for i in range(len(num)):
+    if num[i]<=M:
+        m.append(num[i])
+
+print(max(m))
