2807efac8a744f401b7e5d8916083c807826c0f4
unknown <package9@naver.com>
2020-09-27 23:09:58 +0900

    분해합 못함

---
+++ 2231.py
@@ -0,0 +1,14 @@
+n=input()
+a=[]
+for i in range(len(n)):
+    a.append(int(n[i]))
+
+w=[]
+for i in range(len(a)):
+    w.append(10**i)
+
+
+print(w)
+
+# while n==
+
--- tmp.py
+++
@@ -1,8 +0,0 @@
-a = [[]]
-b = []
-
-for i in range(5):
-    for j in range(5):
-        b[i][j] = j
-
-print(a)
