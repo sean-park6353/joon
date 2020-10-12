9a0be529dfc0f43b8756b304af3149632f1bfd6c
unknown <package9@naver.com>
2020-10-13 01:34:26 +0900

    분해합

--- 2231.py
+++ 2231.py
@@ -1,13 +1,40 @@
-n=input()
-a=[]
+# n = input()
+# arr = []
+
+
+# print(arr)
+# n = int(n)
+# tmp = []
+# for i in range(size):
+#     z = int(n/arr[size-1-i])
+#     n = n-z*(10**(size-i-1))
+#     tmp.append(z)
+
+
+#! 위에는 구린 코드
+n = input()
+size = len(n)
+arr1 = []
+arr2 = []
 for i in range(len(n)):
-    a.append(int(n[i]))
-
-w=[]
-for i in range(len(a)):
-    w.append(10**i)
+    arr1.append(1*(10**i))
 
 
+def divide(s):
+    tmp = []
+    for i in range(len(s)):
+        tmp.append(int(s[i:i+1]))
+    return tmp
 
 
-if 
+cnt = 0
+arr2 = divide(n)
+for i in range(arr1[size-1]*(arr2[0]-1), int(n)):
+    cnt += 1
+    if i+sum(divide(str(i))) == int(n):
+        print(i)
+        break
+
+
+if cnt >= int(n):
+    print(0)
