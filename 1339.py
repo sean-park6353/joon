1a219a9d502a068b4d8d01efe2625c8471ee60c4
unknown <package9@naver.com>
2020-12-20 01:22:33 +0900

    신입 사원

--- 1339.py
+++ 1339.py
@@ -6,3 +6,6 @@ for i in range(n):
     arr.append(list(map(str, input())))
 
 
+sorted(arr, key=lambda x: len(arr))
+
+print(arr)
---
+++ 1541.py
@@ -0,0 +1,5 @@
+n=int(input())
+arr=list(map(int,input().split(' ')))
+while True:
+    for i in range(max(arr)):
+     
