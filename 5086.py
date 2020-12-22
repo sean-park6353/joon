6f4613e19052710bb74fd077e6f1935da9bc18fa
unknown <package9@naver.com>
2020-12-22 14:54:43 +0900

    배수와 약수

---
+++ 5086.py
@@ -0,0 +1,15 @@
+
+arr = []
+while True:
+    a, b = map(int, input().split(' '))
+    if a == 0 and b == 0:
+        break
+    if b % a == 0:
+        arr.append('factor')
+    elif a % b == 0:
+        arr.append('multiple')
+    else:
+        arr.append('neither')
+
+for i in arr:
+    print(i)
