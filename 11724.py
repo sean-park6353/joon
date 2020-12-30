958edd4593324f72906268b3293cb85af7de36d8
unknown <package9@naver.com>
2020-12-30 13:13:08 +0900

    연결 요소의 개수

--- 11724.py
+++ 11724.py
@@ -1,15 +1,26 @@
 
-# def dfs(a, b):
 
-#     print(1)
+def dfs(p):
+    visit[p] = True
+    for i in range(len(graph[p])):
+        if visit[i] == False and graph[p][i] == 1:
+            visit[i] = True
+            dfs(i)
 
 
-# a, b = map(int, input().split(' '))
-# graph = [[0]*a for i in range(a)]
-# visit = [False for i in range(a)]
-# for i in range(b):
-#     data = list(map(int, input().split(' ')))
-#     graph[data[0]-1][data[1]-1] = 1
-#     graph[data[1]-1][data[0]-1] = 1
+a, b = map(int, input().split(' '))
+graph = [[0]*a for p in range(a)]
+visit = [False for p in range(a)]
 
-# for i in range(a):
+
+for j in range(b):
+    data = list(map(int, input().split(' ')))
+    graph[data[1]-1][data[0]-1] = 1
+    graph[data[0]-1][data[1]-1] = 1
+result = []
+cnt = 0
+for i in range(a):
+    if visit[i] != True:
+        dfs(i)
+        cnt += 1
+print(cnt)
