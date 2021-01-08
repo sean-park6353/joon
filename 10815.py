4fd4cb637c486fbeb84c2344e8e43a539247a541
unknown <package9@naver.com>
2021-01-09 00:01:40 +0900

    숫자 카드

--- 10815.py
+++ 10815.py
@@ -1,19 +1,11 @@
-
 import sys
-n = int(sys.stdin.readline())
-arr = list(map(int, sys.stdin.readline().split()))
-k = int(sys.stdin.readline())
-brr = list(map(int, sys.stdin.readline().split()))
-
-
-
-# tmp = set(arr)-(set(arr)-(set(brr)))
-# value = list(tmp)
-# result = [0 for i in range(k)]
-# z = []
-# for i in range(len(value)):
-#     z.append(brr.index(value[i]))
-# for i in range(len(z)):
-#     result[z[i]] += 1
-# print(*result, sep=' ')
-284,000
+a = int(sys.stdin.readline())
+arr = set(list(map(int, sys.stdin.readline().split(' '))))
+b = int(sys.stdin.readline())
+brr = list(map(int, sys.stdin.readline().split(' ')))
+for i in brr:
+    if i in arr:
+        sys.stdout.write('1')
+    else:
+        sys.stdout.write('0')
+    sys.stdout.write(' ')
