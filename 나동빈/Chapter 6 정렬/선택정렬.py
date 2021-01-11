7b6c78c74fad7b0e66d02844a829825757a7d53e
unknown <package9@naver.com>
2021-01-11 17:30:53 +0900

    위에서 아래로?

---
+++ 나동빈/Chapter 6 정렬/선택정렬.py
@@ -0,0 +1,9 @@
+
+a=[15,7,1,4,2]
+
+for i in range(len(a)):
+    for j in range(i+1,len(a)):
+        if a[i]>a[j]:
+            a[i],a[j]=a[j],a[i]
+
+print(a)
---
+++ 나동빈/Chapter 6 정렬/위에서 아래로.py
@@ -0,0 +1,5 @@
+a = int(input())
+arr = []
+for i in range(a):
+    arr.append(int(input()))
+print(sorted(arr))
