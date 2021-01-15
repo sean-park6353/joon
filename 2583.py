e3c0435342b77d1d85239b5ef60d4f22ae453836
unknown <package9@naver.com>
2021-01-15 23:01:48 +0900

    배열 합치기

---
+++ 2583.py
@@ -0,0 +1,7 @@
+a, b, c = map(int, input().split(" "))
+arr = []
+graph = [[0]*b for i in range(a)]
+for i in range(c):
+    leftx, lefty, rightx, righty = map(int, input().split(' '))
+    for j in range(rightx-leftx):
+        graph[]
