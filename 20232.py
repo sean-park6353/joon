80257812470c7179403da25af71cd15772961c27
unknown <package9@naver.com>
2021-01-17 13:25:36 +0900

    Archivist

--- 20232.py
+++ 20232.py
@@ -1,7 +1,52 @@
 arr = ['ITMO', 'SPbSU', 'PetrSU']
-n = int(input())
-for i in range(1995, 2020):
-    if n == i:
-        print(arr[0])
-        break
-    elif n == 199
+i = int(input())
+if i == 1995:
+    print(arr[0])
+elif i == 1996:
+    print(arr[1])
+elif i == 1997:
+    print(arr[1])
+elif i == 1998:
+    print(arr[0])
+elif i == 1999:
+    print(arr[0])
+elif i == 2000:
+    print(arr[1])
+elif i == 2001:
+    print(arr[0])
+elif i == 2002:
+    print(arr[0])
+elif i == 2003:
+    print(arr[0])
+elif i == 2004:
+    print(arr[0])
+elif i == 2005:
+    print(arr[0])
+elif i == 2006:
+    print("%s, %s" % (arr[2], arr[0]))
+elif i == 2007:
+    print(arr[1])
+elif i == 2008:
+    print(arr[1])
+elif i == 2009:
+    print(arr[0])
+elif i == 2010:
+    print(arr[0])
+elif i == 2011:
+    print(arr[0])
+elif i == 2012:
+    print(arr[0])
+elif i == 2013:
+    print(arr[1])
+elif i == 2014:
+    print(arr[0])
+elif i == 2015:
+    print(arr[0])
+elif i == 2016:
+    print(arr[0])
+elif i == 2017:
+    print(arr[0])
+elif i == 2018:
+    print(arr[1])
+else:
+    print(arr[0])
