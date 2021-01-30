8deb35df333fdebc7b84b031f0db5027783c67b4
park <package9@naver.com>
2021-01-30 23:17:47 +0900

    미로탐색 복습

--- 연습장.py
+++ 연습장.py
@@ -1,18 +1,31 @@
-# n=9
-# arr=[0,12345678,1,2,0,0,0,0,32]
-import heapq,sys
-arr=[]
-result=[]
-heapq.heapify(result)
-n=int(sys.stdin.readline())
-for i in range(n):
-    arr.append(int(sys.stdin.readline()))
-for i in range(n):
-    if arr[i]==0:
-        if len(result)==0:
-            print(0)
-            continue
-        else:
-            print(-heapq.heappop(result))   
-    else:
-        heapq.heappush(result,-arr[i])
+from collections import deque
+
+a,b=map(int,input().split())
+xpos=[-1,1,0,0]
+ypos=[0,0,-1,1]
+graph=[]
+visit=[[0]*b for _ in range(a)]
+
+
+def bfs(x,y):
+    q=deque([(x,y)])
+    while q:
+        x,y=q.popleft()
+        for i in range(4):
+            new_x=x+xpos[i]
+            new_y=y+ypos[i]
+            if new_x<0 or new_y<0 or new_x>=a or new_y>=b:
+                continue
+            if graph[new_x][new_y]!=1:
+                continue
+            if visit[new_x][new_y]!=0:
+                continue
+            visit[new_x][new_y]=visit[x][y]+1
+            q.append((new_x,new_y))
+    print(visit[a-1][b-1])
+for i in range(a):
+    graph.append(list(map(int,input().strip())))
+
+
+visit[0][0]=1
+bfs(0,0)
