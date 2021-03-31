f51d335ef90c36e74c922ac9f44a1b19a7c70812
park <package9@naver.com>
2021-03-31 19:44:20 +0900

    Cakes

--- 21212.py
+++ 21212.py
@@ -1,3 +1,16 @@
 n=int(input())
+arr=[]
 for i in range(n):
-    g
+    arr.append(list(map(int,input().split())))
+flg=False
+cnt=0
+answer=[]
+for ingredient,needs in arr:
+    if ingredient> needs:
+        print(0)
+        flg=True
+        break
+    cakes=needs//ingredient
+    answer.append(cakes)
+if flg==False:
+    print(min(answer))
